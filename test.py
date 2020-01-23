33df98119dd23ff98127860169ee88d461e2a2e5
china-zhangdj <1332968751@qq.com>
2020-01-23 16:04:06 +0800

    add a python test code .

---
+++ test.py
@@ -0,0 +1,17 @@
+import ctypes
+import sys
+
+dll_file = "cmake-build-debug/libmotion.so"
+
+motion_sensor = ctypes.CDLL(dll_file)
+
+motion_sensor.motion_setup()
+
+
+while True :
+    motion_sensor.motion_update()
+    roll = ctypes.c_float.in_dll(motion_sensor,"roll")
+    print(roll.value)
+
+
+motion_sensor.motion_close()
